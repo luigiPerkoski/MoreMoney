742705520b25d316fd685246b0b13367caf7cfad
Luigi Perkoski <celluigipplc@gmail.com>
2023-09-17 02:22:14 -0300

    task forms edit descriptions

--- core/forms.py
+++ core/forms.py
@@ -7,22 +7,30 @@ from .models import Extract, Account
 #// Formulario para adicional novos objetos nos meus modelos 
 
 class NewExtract(forms.ModelForm):
+    
     class Meta:
         model = Extract
-        fields = ('name', 'value', 'account', 'type', 'date', 'descripition', 'pay')
+        fields = ('name', 'value', 'account', 'type', 'date', 'pay')
 
         widgets = {
             'date': forms.DateInput(attrs={'type': 'date'}),
+            'value': forms.TextInput(attrs={'type': 'text'}),
         }
+
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}), required=False)
+
+        
+    
 
 class NewAccount(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ('name', 'value', 'type', 'descripition')
+        fields = ('name', 'value', 'type')
 
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'})
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'value': forms.TextInput(attrs={'type': 'text'}),
             }
-        
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 4}), required=False)
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100)
